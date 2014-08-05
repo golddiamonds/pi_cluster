9aaf5b2b4e5e352565bd32278bdecd54cde47a57
golddiamonds <rhall@berkeley.edu>
2014-08-05 19:28:46 +0000

    Adding bash file to keep things sync'd across pi instances.

---
+++ bash_git.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+START_HERE="/home/pi/pi_cluster/";
+
+cd $START_HERE;
+
+git pull
+
+echo "PULLED!";
+
+GO_HERE="/home/pi/";
+
+cd $GO_HERE
+
+cp /home/pi/pi_cluster/bash_git.sh /home/pi/bash_git.sh
+
+chmod 755 bash_git.sh
