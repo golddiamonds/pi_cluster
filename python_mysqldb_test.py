a83c81d5fc5b4bb1bf0b4b0fcef183d1184495ce
golddiamonds <rhall@berkeley.edu>
2014-08-06 17:03:19 +0000

    Pis now save temperature data to database.

--- python_mysqldb_test.py
+++ python_mysqldb_test.py
@@ -7,6 +7,10 @@ import commands
 ip = commands.getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:]
 print ip
 
+#grab temp
+temp = commands.getoutput("/opt/vc/bin/vcgencmd measure_temp")[5:-2]
+print temp
+
 #connect to database
 db = MySQLdb.connect(host="192.168.0.111",user="root",passwd="drowssap",db="pi_cluster")
 
@@ -14,6 +18,6 @@ db = MySQLdb.connect(host="192.168.0.111",user="root",passwd="drowssap",db="pi_c
 curs = db.cursor()
 
 #insert into the log
-curs.execute("INSERT INTO log (ip, message) VALUES (%s, 'Test')", (str(ip),))
+curs.execute("INSERT INTO log (ip, message, temp) VALUES (%s, 'Test', %s)", (str(ip),str(temp),))
 
 db.commit() 
